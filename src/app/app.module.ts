d25f01f27ba03f68f1ded9cae1f8b8a2ac631503
Programador1 <programador1@comedal.com.co>
2019-09-25 17:48:50 -0500

    Inicio del Proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
+import { PedidoComponent } from './pedidos/pedido/pedido.component';
+import { PedidoItemsComponent } from './pedidos/pedido-items/pedido-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PedidosComponent,
+    PedidoComponent,
+    PedidoItemsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pedidos/pedido-items/pedido-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pedido-items',
+  templateUrl: './pedido-items.component.html',
+  styles: []
+})
+export class PedidoItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
