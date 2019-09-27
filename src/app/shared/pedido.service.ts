7009116efe507d71ef8c848cfaed958a162a1cb7
Victor <victor.ossa@hotmail.com>
2019-09-27 07:04:32 -0500

    Inicio de Tabla

--- src/app/shared/pedido.service.ts
+++ src/app/shared/pedido.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Pedido } from './pedido.model';
+import { PedidoItem } from './pedido-item.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PedidoService {
   formData:Pedido;
+  pedidosItems:PedidoItem[];
 
   constructor() { }
 }
