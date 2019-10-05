6ec35d902008c43e01105fb9d50a83903f8d087f
Victor <victor.ossa@hotmail.com>
2019-10-05 17:12:17 -0500

    Se agrega Funcionalidad GTotal

--- src/app/pedidos/pedido/pedido.component.ts
+++ src/app/pedidos/pedido/pedido.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PedidoService } from 'src/app/shared/pedido.service';
 import { NgForm } from '@angular/forms';
 import { Alert } from 'selenium-webdriver';
-import { MatDialog,MatDialogConfig } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { PedidoItemsComponent } from '../pedido-items/pedido-items.component';
 
 @Component({
@@ -12,37 +12,48 @@ import { PedidoItemsComponent } from '../pedido-items/pedido-items.component';
 })
 export class PedidoComponent implements OnInit {
 
-  constructor(private service:PedidoService,private dialog:MatDialog ) { }
+  constructor(private service: PedidoService, private dialog: MatDialog) { }
 
   ngOnInit() {
     this.resetForm();
   }
 
-  resetForm(form?:NgForm){
-    if(form = null)
-    form.resetForm();
+  resetForm(form?: NgForm) {
+    if (form = null)
+      form.resetForm();
     this.service.formData = {
-      PedidoID:null,
-      PedidoNo:Math.floor(10000+Math.random()*900000).toString(),
-      ClienteID:0,
-      PMetodo:'',
-      Gtotal:0 
+      PedidoID: null,
+      PedidoNo: Math.floor(10000 + Math.random() * 900000).toString(),
+      ClienteID: 0,
+      PMetodo: '',
+      Gtotal: 0
     };
     this.service.pedidosItems = [];
 
   }
 
-  AgregarOEditarPedido(pedidoItemIndex,PedidoID){
+  AgregarOEditarPedido(pedidoItemIndex, PedidoID) {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.autoFocus = true;
     dialogConfig.disableClose = true;
-    dialogConfig.width="50%";
-    dialogConfig.data = {pedidoItemIndex,PedidoID}
-    this.dialog.open(PedidoItemsComponent,dialogConfig); 
+    dialogConfig.width = "50%";
+    dialogConfig.data = { pedidoItemIndex, PedidoID }
+    this.dialog.open(PedidoItemsComponent, dialogConfig).afterClosed().subscribe(res=> {
+      this.updateGrandTotal();
+    });
   }
 
-  onDeletePedidoItem(pedidoItemID:number, i:number){
-    this.service.pedidosItems.splice(i,1);
+  onDeletePedidoItem(pedidoItemID: number, i: number) {
+    this.service.pedidosItems.splice(i, 1);
+    this.updateGrandTotal();
+  }
+
+
+  updateGrandTotal() {
+    this.service.formData.Gtotal = this.service.pedidosItems.reduce((prev, curr) => {
+      return prev + curr.Total;      
+    }, 0);
+    this.service.formData.Gtotal = parseFloat(this.service.formData.Gtotal.toFixed(2));
   }
 
 }
